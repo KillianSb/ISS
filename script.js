9ca02f50a347a2b18d2f9eb44af559319c1eefb4
Killian.Sb <TheKller29@Gmail.com>
2023-07-26 18:38:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function affichageMap(json) {
 
 Particles.init({
     selector: '.particules',
-    color: ['#ff0000', '#ffffff', '#666666'],
+    color: ['#0051ff', '#ffffff'],
     maxParticles: 300,
     speed: 0.8,
     connectParticles: true,
